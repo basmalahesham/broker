3291fff92c78d391dfe113d8572980fefe3d1dbd
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-08-31 02:19:03 +0300

    create bottomNavigationBar

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,11 +1,76 @@
+import 'package:broker/constants.dart';
 import 'package:flutter/material.dart';
 
-class HomeLayout extends StatelessWidget {
-  const HomeLayout({super.key});
+class HomeLayout extends StatefulWidget {
+  HomeLayout({super.key});
 
   static const String routeName = "home_view";
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int selectedIndex = 0;
+
+  List<Widget> screenWidgets = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      //body: screenWidgets[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/home.png'),
+            ),
+            label: 'الرئيسية',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/map.png'),
+            ),
+            label: 'الخريطة',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/add.png'),
+            ),
+            label: 'اضافة',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/order.png'),
+            ),
+            label: 'طلباتي',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/Message.png'),
+            ),
+            label: 'الرسائل',
+          ),
+        ],
+        selectedLabelStyle: TextStyle(
+          color: Colors.red,
+          fontWeight: FontWeight.w500,
+          fontSize: 11,
+        ),
+        unselectedLabelStyle: TextStyle(
+          color: Colors.black,
+        ),
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.black,
+        selectedIconTheme: IconThemeData(color: Colors.red),
+        unselectedIconTheme: IconThemeData(color: Colors.black),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:broker/constants.dart';
 import 'package:broker/features/splash/presentation/views/splash_view1.dart';
 import 'package:broker/features/splash/presentation/views/splash_view2.dart';
 import 'package:broker/layout/home_layout.dart';
@@ -16,11 +17,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
       initialRoute: SplashView1.routeName,
       routes: {
         SplashView1.routeName: (context) => SplashView1(),
-        SplashView2.routeName : (context) => SplashView2(),
-        HomeLayout.routeName : (context) => HomeLayout(),
+        SplashView2.routeName: (context) => SplashView2(),
+        HomeLayout.routeName: (context) => HomeLayout(),
       },
     );
   }
