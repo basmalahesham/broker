d113fc53e0505474e37109a8d33654fd62f4fd8b
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-01 00:29:37 +0300

    create home_view.dart & map_view.dart & add_view.dart & orders_view.dart & message_view.dart

--- lib/features/layout/home_layout.dart
+++ lib/features/layout/home_layout.dart
@@ -1,4 +1,9 @@
 import 'package:broker/core/utils/styles.dart';
+import 'package:broker/features/add/presentation/views/add_view.dart';
+import 'package:broker/features/home/presentation/views/home_view.dart';
+import 'package:broker/features/map/presentation/views/map_view.dart';
+import 'package:broker/features/messages/presentation/views/message_view.dart';
+import 'package:broker/features/orders/presentation/views/orders_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeLayout extends StatefulWidget {
@@ -13,12 +18,18 @@ class HomeLayout extends StatefulWidget {
 class _HomeLayoutState extends State<HomeLayout> {
   int selectedIndex = 0;
 
-  List<Widget> screenWidgets = [];
+  List<Widget> screenWidgets = [
+    const HomeView(),
+    const MapView(),
+    const AddView(),
+    const OrdersView(),
+    const MessageView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //body: screenWidgets[selectedIndex],
+      body: screenWidgets[selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: selectedIndex,
         onTap: (index) {
@@ -28,33 +39,23 @@ class _HomeLayoutState extends State<HomeLayout> {
         },
         items: const [
           BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/home.png'),
-            ),
+            icon: ImageIcon(AssetImage('assets/images/home.png')),
             label: 'الرئيسية',
           ),
           BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/map.png'),
-            ),
+            icon: ImageIcon(AssetImage('assets/images/map.png')),
             label: 'الخريطة',
           ),
           BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/add.png'),
-            ),
+            icon: ImageIcon(AssetImage('assets/images/add.png')),
             label: 'اضافة',
           ),
           BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/order.png'),
-            ),
+            icon: ImageIcon(AssetImage('assets/images/order.png')),
             label: 'طلباتي',
           ),
           BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/Message.png'),
-            ),
+            icon: ImageIcon(AssetImage('assets/images/Message.png')),
             label: 'الرسائل',
           ),
         ],
