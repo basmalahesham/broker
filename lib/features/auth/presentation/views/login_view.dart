c155d05029b890812e893853519701521ca15fca
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-11 02:44:26 +0300

    refactor our code

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -22,135 +22,143 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       backgroundColor: const Color(0xFFE8EEEC),
-      body: Form(
-        key: formKey,
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.all(24),
-            margin: const EdgeInsets.all(16),
-            width: 400.w,
-            height: 500.h,
-            decoration: BoxDecoration(
-              color: const Color(0xFFFDFDFD),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'تسجيل الدخول',
-                  style: Styles.textStyle24.copyWith(
-                    fontWeight: FontWeight.normal,
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
 
-                    color: const Color(0xFF332620),
+            child: Center(
+              child: Form(
+                key: formKey,
+                child: Container(
+                  padding: const EdgeInsets.all(24),
+                  margin: const EdgeInsets.all(16),
+                  width: 400.w,
+                  height: 500.h,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFDFDFD),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                ),
-                CustomTextFormField2(
-                  controller: emailController,
-                  hintText: 'ادخل بريدك الالكتروني',
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    var regex = RegExp(
-                        r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)");
-                    if (!regex.hasMatch(value)) {
-                      return 'Enter a valid email address';
-                    }
-                    return null;
-                  },
-                ),
-                CustomTextFormField2(
-                  controller: passwordController,
-                  hintText: '**********',
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    var regex = RegExp(
-                        r"(?=^.{8,}$)(?=.*[!@#$%^&*]+)(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$");
-                    if (!regex.hasMatch(value)) {
-                      return 'enter a valid password';
-                    }
-                    return null;
-                  },
-                ),
-                CustomRowCheck(text: 'تذكرني'),
-                Text(
-                  'هل نسيت كلمة المرور؟',
-                  style: Styles.textStyle16.copyWith(
-                    fontWeight: FontWeight.normal,
-                    color: const Color(0xFFBF554B),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 260.w,
-                      height: 40.h,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFFBF554B),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5),
-                          ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'تسجيل الدخول',
+                        style: Styles.textStyle24.copyWith(
+                          fontWeight: FontWeight.normal,
+
+                          color: const Color(0xFF332620),
                         ),
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            Navigator.pushReplacementNamed(
-                              context,
-                              HomeLayout.routeName,
-                            );
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Success')),
-                            );
+                      ),
+                      CustomTextFormField2(
+                        controller: emailController,
+                        hintText: 'ادخل بريدك الالكتروني',
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'Please enter your email';
                           }
-                        }, // Call the onNext function
-                        child: Text(
-                          'تسجيل دخول',
-                          style: Styles.textStyle14.copyWith(
-                            color: Colors.white,
-                          ),
+                          var regex = RegExp(
+                              r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)");
+                          if (!regex.hasMatch(value)) {
+                            return 'Enter a valid email address';
+                          }
+                          return null;
+                        },
+                      ),
+                      CustomTextFormField2(
+                        controller: passwordController,
+                        hintText: '**********',
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          var regex = RegExp(
+                              r"(?=^.{8,}$)(?=.*[!@#$%^&*]+)(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$");
+                          if (!regex.hasMatch(value)) {
+                            return 'enter a valid password';
+                          }
+                          return null;
+                        },
+                      ),
+                      CustomRowCheck(text: 'تذكرني'),
+                      Text(
+                        'هل نسيت كلمة المرور؟',
+                        style: Styles.textStyle16.copyWith(
+                          fontWeight: FontWeight.normal,
+                          color: const Color(0xFFBF554B),
                         ),
                       ),
-                    ),
-                    Center(
-                      child: SizedBox(
-                        width: 290.w,
-                        height: 40.h,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF1A4B42),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 260.w,
+                            height: 40.h,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFFBF554B),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              onPressed: () {
+                                if (formKey.currentState!.validate()) {
+                                  Navigator.pushReplacementNamed(
+                                    context,
+                                    HomeLayout.routeName,
+                                  );
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text('Success')),
+                                  );
+                                }
+                              }, // Call the onNext function
+                              child: Text(
+                                'تسجيل دخول',
+                                style: Styles.textStyle14.copyWith(
+                                  color: Colors.white,
+                                ),
+                              ),
                             ),
                           ),
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(
-                              context,
-                              PhoneRegisterView.routeName,
-                            );
-                          }, // Call the onNext function
-                          child: Text(
-                            'انشاء حساب',
-                            style: Styles.textStyle14.copyWith(
-                              color: Colors.white,
+                          Center(
+                            child: SizedBox(
+                              width: 290.w,
+                              height: 40.h,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFF1A4B42),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  Navigator.pushReplacementNamed(
+                                    context,
+                                    PhoneRegisterView.routeName,
+                                  );
+                                }, // Call the onNext function
+                                child: Text(
+                                  'انشاء حساب',
+                                  style: Styles.textStyle14.copyWith(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
